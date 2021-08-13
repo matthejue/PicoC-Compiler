3d0e6e55f3e7b4b77f4a7eb4a556549983f4fa92
areotwister <areotwister@gmail.com>
2021-08-13 23:32:58 +0200

    one littel irrelevant change in the test.cpp file

--- .misc/test.cpp
+++ .misc/test.cpp
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-void while_true() { car = 10; }
+void while_true() {
+  {
+    {
+      { int car = 10; }
+    }
+  }
+}
 
 int main(int argc, const char **argv) { while_true(); }
