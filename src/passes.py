1538bcebfb48d8cf413ee8f71291b13c479b3ab4
areotwister <areotwister@gmail.com>
2022-04-13 06:13:44 +0200

    continued reimplementing all passes

--- src/passes.py
+++ src/passes.py
@@ -1,3 +1,14 @@
+from picoc_nodes import NT
+
+
 class PicoCCompiler(object):
-    def remove_complex_opds(self):
+    def remove_exp(self):
         ...
+
+    def remove_stmt(self):
+        ...
+
+    def remove_complex_opds(self, f: NT.File):
+        match f:
+            case NT.File():
+                ...
