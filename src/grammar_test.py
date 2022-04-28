d92d33738bc6feb72cd0849d8edb734de8b81a20
areotwister <areotwister@gmail.com>
2022-04-28 02:23:26 +0200

    continued with_lark_parser

--- src/grammar_test.py
+++ src/grammar_test.py
@@ -4,4 +4,13 @@ from lark import Lark
 
 with open("./src/parser_spec.lark") as fin:
     parser = Lark(fin.read(), start="picoc")
-    print(parser.parse("print(_fun120 + 120 * 'c')").pretty())
+    print(
+        parser.parse(
+            r"""
+test
+int test(){
+    print(_fun120 + 120 * -'c')
+}
+    """
+        ).pretty()
+    )
