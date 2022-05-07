cd3e2f908798187078662c136eb3569ba7d46693
areotwister <areotwister@gmail.com>
2022-05-07 17:36:09 +0200

    wrote extensive tests

--- src/errors.py
+++ src/errors.py
@@ -5,17 +5,23 @@ class Errors:
     class InvalidCharacterError(Exception):
         """If there're Token sequences generated from the input that are not
         permitted by the grammar rules"""
+
         def __init__(self, found, found_pos):
-            self.description = f"{CM().YELLOW}InvalidCharacterError{CM().RESET}: '{found}' is not a "\
+            self.description = (
+                f"{CM().YELLOW}InvalidCharacterError{CM().RESET}: '{found}' is not a "
                 "permitted character"
+            )
             self.found = found
             self.found_pos = found_pos
 
     class UnclosedCharacterError(Exception):
         """If a character has a opening apostrophe but not a closing one"""
+
         def __init__(self, expected, found, found_pos):
-            self.description = f"{CM().YELLOW}UnclosedCharacterError{CM().RESET}: Expected {expected},"\
+            self.description = (
+                f"{CM().YELLOW}UnclosedCharacterError{CM().RESET}: Expected {expected},"
                 f" found {found}"
+            )
             self.expected = expected
             self.found = found
             self.found_pos = found_pos
@@ -23,19 +29,25 @@ class Errors:
     class NoApplicableRuleError(Exception):
         """If no rule is applicable in a situation where several undistinguishable
         alternatives are possible"""
+
         def __init__(self, expected, found, found_pos):
-            self.description = f"{CM().YELLOW}NoApplicableRuleError{CM().RESET}: Expected '{expected}'"\
+            self.description = (
+                f"{CM().YELLOW}NoApplicableRuleError{CM().RESET}: Expected '{expected}'"
                 f", found '{found}'"
+            )
             self.expected = expected
             self.found = found
             self.found_pos = found_pos
 
     class MismatchedTokenError(Exception):
         """If Token shouldn't syntactically appear at this position"""
+
         def __init__(self, expected, found, found_pos):
             # there can be several expected and these already have single quotes
-            self.description = f"{CM().YELLOW}MismatchedTokenError{CM().RESET}: Expected '{expected}'"\
+            self.description = (
+                f"{CM().YELLOW}MismatchedTokenError{CM().RESET}: Expected '{expected}'"
                 f", found '{found}'"
+            )
             self.expected = expected
             self.found = found
             self.found_pos = found_pos
@@ -43,22 +55,28 @@ class Errors:
     class TastingError(Exception):
         """This error can only appear while tasting and should be raised if a
         Token appears that can only be part of the other tasting choice"""
-        def __init__(self, ):
+
+        def __init__(
+            self,
+        ):
             self.description = "This error should never be visible"
 
     class UnknownIdentifierError(Exception):
         """If Token shouldn't syntactically appear at this position"""
+
         def __init__(self, found, found_pos):
-            self.description = f"{CM().YELLOW}UnknownIdentifierError{CM().RESET}: Identifier "\
+            self.description = (
+                f"{CM().YELLOW}UnknownIdentifierError{CM().RESET}: Identifier "
                 f"'{found}' wasn't declared yet"
+            )
             self.found = found
             self.found_pos = found_pos
 
     class TooLargeLiteralError(Exception):
         """If the literal assigned to a variable is too large for the datatype of
         the variable"""
-        def __init__(self, found, found_pos, found_symbol_type, found_from,
-                     found_to):
+
+        def __init__(self, found, found_pos, found_symbol_type, found_from, found_to):
             self.description = f"{CM().YELLOW}TooLargeLiteralError{CM().RESET}: Literal '{found}' is too large"
             self.found = found
             self.found_pos = found_pos
@@ -68,7 +86,9 @@ class Errors:
 
     class RedefinitionError(Exception):
         def __init__(self, found, found_pos, first, first_pos):
-            self.description = f"{CM().YELLOW}RedefinitionError{CM().RESET}: Redefinition of '{found}'"
+            self.description = (
+                f"{CM().YELLOW}RedefinitionError{CM().RESET}: Redefinition of '{found}'"
+            )
             self.found = found
             self.found_pos = found_pos
             self.first = first
@@ -76,8 +96,10 @@ class Errors:
 
     class ConstReassignmentError(Exception):
         def __init__(self, found, found_pos, first, first_pos):
-            self.description = f"{CM().YELLOW}ConstReassignmentError{CM().RESET}: Can't reassign a new "\
+            self.description = (
+                f"{CM().YELLOW}ConstReassignmentError{CM().RESET}: Can't reassign a new "
                 f"value to named constant '{found}'"
+            )
             self.found = found
             self.found_pos = found_pos
             self.first = first
@@ -85,19 +107,27 @@ class Errors:
 
     class NoMainFunctionError(Exception):
         """If there's no main function within the given file"""
+
         def __init__(self, fname):
-            self.description = f"{CM().YELLOW}NoMainFunctionError{CM().RESET}: There's no main function"\
+            self.description = (
+                f"{CM().YELLOW}NoMainFunctionError{CM().RESET}: There's no main function"
                 f" in file '{fname}'"
+            )
 
     class MoreThanOneMainFunctionError(Exception):
         def __init__(self, first_pos, second_pos):
-            self.description = f"{CM().YELLOW}MoreThanOneMainFunctionError{CM().RESET}: There're at "\
+            self.description = (
+                f"{CM().YELLOW}MoreThanOneMainFunctionError{CM().RESET}: There're at "
                 "least two main functions"
+            )
             self.first_pos = first_pos
             self.second_pos = second_pos
 
     class NotImplementedYetError(Exception):
         """Feature that isn't implemented yet"""
+
         def __init__(self, feature_description):
-            self.description = f"{CM().YELLOW}NotImplementedYet{CM().RESET}: The feature of using "\
+            self.description = (
+                f"{CM().YELLOW}NotImplementedYet{CM().RESET}: The feature of using "
                 f"{feature_description} is not implemented yet"
+            )
