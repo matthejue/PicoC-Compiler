2fa180e8dee9b04eaf059ee2dd5394e9f03a092f
areotwister <areotwister@gmail.com>
2022-05-10 04:42:57 +0200

    improved run_tests.sh script

--- convert_to_c.py
+++ convert_to_c.py
@@ -4,5 +4,22 @@
 import sys
 import global_vars
 
-with open(global_vars.outbase + ".tokens", "r", encoding="utf-8") as fin:
+with open(glo + ".tokens", "r", encoding="utf-8") as fin:
     picoc_input = fin.read()
+
+
+def remove_extension(fname):
+    """stips of the file extension
+    :fname: filename
+    :returns: basename of the file
+
+    """
+    # if there's no '.' rindex raises a exception, rfind returns -1
+    index_of_extension_start = fname.rfind(".")
+    if index_of_extension_start == -1:
+        return fname
+    return fname[0:index_of_extension_start]
+
+
+def subheading(heading, terminal_width, symbol):
+    return f"{symbol * ((terminal_width - len(heading) - 2) // 2 + (1 if (terminal_width - len(heading)) % 2 else 0))} {heading} {symbol * ((terminal_width - len(heading) - 2) // 2)}"
